038015b8cf44835eb5c371c6258d215335f043ff
IssatayTleuov <issatai84@gmail.com>
2023-02-19 12:04:35 +0600

    Updated placeAircraft method

--- src/Main.java
+++ src/Main.java
@@ -42,14 +42,35 @@ public class Main {
         }
     }
 
-    public static boolean placeAircraft() {
-        printBattlefield();
-        System.out.println("Enter the coordinates of the Aircraft Carrier (5 cells):");
-        try (InputStreamReader reader1 = new InputStreamReader(System.in);
-             BufferedReader reader2 = new BufferedReader(reader1)) {
-            String input = reader2.readLine();
-        } catch (Exception e) {
-            throw new RuntimeException(e.getMessage());
+    public static void placeAircraft() {
+        boolean isShipPlaced = false;
+        while (!isShipPlaced) {
+            try (InputStreamReader reader1 = new InputStreamReader(System.in);
+                 BufferedReader reader2 = new BufferedReader(reader1)) {
+                printBattlefield();
+                System.out.println("Enter the coordinates of the Aircraft Carrier (5 cells):");
+                char[] coordinates = reader2.readLine().toCharArray();
+                int[] arrayIndexes = new int[4];
+                for (int i = 0; i < coordinates.length - 1; i++) {
+                    if (battlefieldIndex.containsKey(String.valueOf(coordinates[i]))) {
+                        arrayIndexes[i] = battlefieldIndex.get(String.valueOf(coordinates[i]));
+                    } else {
+                        arrayIndexes[i] = coordinates[i];
+                    }
+
+                    if (arrayIndexes[0] == arrayIndexes[2] || arrayIndexes[1] == arrayIndexes[3]) {
+                        if (arrayIndexes[0] - arrayIndexes[2] + 1 == 5) {
+
+                        } else if (arrayIndexes[1] arrayIndexes[3]) {
+
+                        }
+                    } else {
+                        System.out.println("Error! Wrong ship location! Try again:");
+                    }
+                }
+            } catch (Exception e) {
+                throw new RuntimeException(e.getMessage());
+            }
         }
     }
 }
